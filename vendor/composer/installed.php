5831ee5a8cbeef0fdc6af7a0a664fabb88e54212
Karina <KARINA>
2025-04-11 23:20:01 +0300

    фикс

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'serg/repo',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'cb16468df2fa443e6fa5a7922d2a94bf762c6f69',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'serg/repo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'cb16468df2fa443e6fa5a7922d2a94bf762c6f69',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
